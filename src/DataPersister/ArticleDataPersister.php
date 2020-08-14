4c74478c1e64a75d64d21177d606f031f97de714
Ousmane DIOP <ousmanediopp268@gmail.com>
2020-08-14 13:16:00 +0000

    update datapersister to the update date

--- src/DataPersister/ArticleDataPersister.php
+++ src/DataPersister/ArticleDataPersister.php
@@ -6,6 +6,7 @@ namespace App\DataPersister;
 
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
 
@@ -19,13 +20,16 @@ class ArticleDataPersister implements ContextAwareDataPersisterInterface
      */
     private $_entityManager;
     private $_slugger;
+    private $_request;
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        SluggerInterface $slugger
+        SluggerInterface $slugger,
+        RequestStack $request
     ) {
         $this->_entityManager = $entityManager;
         $this->_slugger = $slugger;
+        $this->_request = $request->getCurrentRequest();
     }
 
 
@@ -39,9 +43,17 @@ class ArticleDataPersister implements ContextAwareDataPersisterInterface
      */
     public function persist($data, array $context = [])
     {
-        $data->setSlug(
-            $this->_slugger->slug(strtolower($data->getTitle())) . '-' . uniqid()
-        );
+        // Update the slug only if the article isn't published
+        if (!$data->getIsPublished()) {
+            $data->setSlug(
+                $this->_slugger->slug(strtolower($data->getTitle())) . '-' . uniqid()
+            );
+        }
+
+        // Set the updatedAt value if it's not a POST request
+        if ($this->_request->getMethod() !== 'POST') {
+            $data->setUpdatedAt(new \DateTime());
+        }
 
         $this->_entityManager->persist($data);
         $this->_entityManager->flush();
